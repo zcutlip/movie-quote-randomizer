8aead8af789020f583198f105835aa5718ec0eea
Zachary Cutlip <uid000@gmail.com>
2025-06-10 21:10:58 -0700

    add quote_types module

---
+++ mq_gen/quote_types.py
@@ -0,0 +1,46 @@
+mgquote_type_registry = {}
+
+
+def register_quote_type(quote_class):
+    """
+    Decorator to register quote classes
+    """
+    quote_type = quote_class.QUOTE_TYPE
+    mgquote_type_registry[quote_type] = quote_class
+    return quote_class
+
+
+@register_quote_type
+class MGQuoteSingle(dict):
+    QUOTE_TYPE = "single"
+
+    def __init__(self, quote_dict: dict):
+        super().__init__()
+        if quote_dict["quote_type"] != self.QUOTE_TYPE:
+            raise ValueError(f"Invalid quote type: {quote_dict["quote_type"]}")
+        self.update(quote_dict)
+
+    def characters(self) -> list[str]:
+        return self["characters"]
+
+    def lines(self) -> list[dict[str, str]]:
+        line = {self._character(): self._line()}
+        return [line]
+
+    def _line(self) -> str:
+        return self["line"]
+
+    def _character(self) -> str:
+        return self.characters()[0]
+
+
+@register_quote_type
+class MGQuoteDialogue(MGQuoteSingle):
+    QUOTE_TYPE = "dialogue"
+
+    def __init__(self, quote_dict: dict):
+        super().__init__(quote_dict)
+
+    def lines(self) -> list[dict[str, str]]:
+        lines = self["lines"]
+        return lines
