4ac78b0429216b8c694992fd04018fff17f9baae
Zachary Cutlip <uid000@gmail.com>
2025-06-17 20:23:58 -0700

    refactor setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,33 @@ with open("mq_randomizer/__about__.py") as fp:
 with open("README.md") as fp:
     long_description = fp.read()
 
-setup(name='mq-generator',
-      version=about["__version__"],
-      description=about["__summary__"],
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author="Zachary Cutlip",
-      author_email="uid000@gmail.com",
-      url="TBD",
-      license="MIT",
-      packages=find_packages(),
-      entry_points={
-          'console_scripts': ['mq-gen=mq_gen.cli:main'], },
-      python_requires='>=3.10',
-      install_requires=[],
-      package_data={'mq_gen': ['config/*']},
-      )
+packages = find_packages(
+    where=".", include=["mq_randomizer", "mq_randomizer.*"]
+)
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Zachary Cutlip",
+    author_email="uid000@gmail.com",
+    url="TBD",
+    license="MIT",
+    packages=find_packages(),
+    package_data={'mq_randomizer': ['py.typed'],
+                  'mq_randomizer.data': ['**/*.json']},
+    python_requires='>=3.10',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities"
+    ],
+)
