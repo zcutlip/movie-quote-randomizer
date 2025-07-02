cf5d679ee8c0273eebf9dc0f9ca439ad586d0be9
Zachary Cutlip <uid000@gmail.com>
2025-07-01 17:17:35 -0700

    add tests for MQuoteLine equality

--- tests/test_quote_types.py
+++ tests/test_quote_types.py
@@ -27,6 +27,32 @@ class TestMQuote:
         sample_quote_line = MQuoteLine("Neo", "Remember... there is no spoon.")
         assert sample_quote.lines == [sample_quote_line]
 
+    def test_mquote_line_equality_010(self):
+        """
+        Test the __eq__() implementation
+
+        Verify: two different MQuoteLine but equivalent objects are equal
+        """
+        quote_line_1 = MQuoteLine("Neo", "Remember... there is no spoon.")
+        quote_line_2 = MQuoteLine("Neo", "Remember... there is no spoon.")
+
+        # no tricks up our sleeve ,these actually are different objects
+        assert id(quote_line_1) != id(quote_line_2)
+        assert quote_line_1 == quote_line_1
+
+    def test_mquote_line_equality_020(self):
+        """
+        Test the __eq__() implementation
+
+        Verify: An MQuoteLine object is equal to itself
+
+        Note: this is trivially true and is present for the purpose of complete coverage
+        """
+        quote_line = MQuoteLine("Neo", "Remember... there is no spoon.")
+
+        assert id(quote_line) == id(quote_line)
+        assert quote_line == quote_line
+
     def test_str_representation(self, sample_quote):
         expected = "Neo: Remember... there is no spoon."
         assert str(sample_quote) == expected
