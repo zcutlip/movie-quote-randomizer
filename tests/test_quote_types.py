d4ba627384511d1c794b8b5fb5ea797bf51fecc9
Zachary Cutlip <uid000@gmail.com>
2025-07-01 19:11:17 -0700

    make all quote field values class attributes

--- tests/test_quote_types.py
+++ tests/test_quote_types.py
@@ -4,27 +4,33 @@ from mq_randomizer.quote_types import MQuote, MQuoteLine
 
 
 class TestMQuote:
+    _media_title = "The Matrix"
+    _media_type = "movie"
+    _media_year = 1999
+    _characters = ["Neo"]
+    _neo_line = "Remember... there is no spoon."
+
     @pytest.fixture
     def sample_quote(self):
         quote_dict = {
-            "characters": ["Neo"],
-            "lines": [{"Neo": "Remember... there is no spoon."}]
+            "characters": self._characters,
+            "lines": [{"Neo": self._neo_line}]
         }
-        return MQuote(quote_dict, "The Matrix", "movie", 1999)
+        return MQuote(quote_dict, self._media_title, self._media_type, self._media_year)
 
     def test_initialization(self, sample_quote):
-        assert sample_quote["media_title"] == "The Matrix"
-        assert sample_quote["media_type"] == "movie"
-        assert sample_quote["year"] == 1999
-        assert sample_quote["characters"] == ["Neo"]
-        sample_quote_line = MQuoteLine("Neo", "Remember... there is no spoon.")
+        assert sample_quote["media_title"] == self._media_title
+        assert sample_quote["media_type"] == self._media_type
+        assert sample_quote["year"] == self._media_year
+        assert sample_quote["characters"] == self._characters
+        sample_quote_line = MQuoteLine("Neo", self._neo_line)
         assert sample_quote["lines"] == [sample_quote_line]
 
     def test_characters_property(self, sample_quote):
         assert sample_quote.characters == ["Neo"]
 
     def test_lines_property(self, sample_quote):
-        sample_quote_line = MQuoteLine("Neo", "Remember... there is no spoon.")
+        sample_quote_line = MQuoteLine("Neo", self._neo_line)
         assert sample_quote.lines == [sample_quote_line]
 
     def test_mquote_line_equality_010(self):
@@ -33,8 +39,8 @@ class TestMQuote:
 
         Verify: two different MQuoteLine but equivalent objects are equal
         """
-        quote_line_1 = MQuoteLine("Neo", "Remember... there is no spoon.")
-        quote_line_2 = MQuoteLine("Neo", "Remember... there is no spoon.")
+        quote_line_1 = MQuoteLine("Neo", self._neo_line)
+        quote_line_2 = MQuoteLine("Neo", self._neo_line)
 
         # no tricks up our sleeve ,these actually are different objects
         assert id(quote_line_1) != id(quote_line_2)
@@ -48,13 +54,13 @@ class TestMQuote:
 
         Note: this is trivially true and is present for the purpose of complete coverage
         """
-        quote_line = MQuoteLine("Neo", "Remember... there is no spoon.")
+        quote_line = MQuoteLine("Neo", self._neo_line)
 
         assert id(quote_line) == id(quote_line)
         assert quote_line == quote_line
 
     def test_str_representation(self, sample_quote):
-        expected = "Neo: Remember... there is no spoon."
+        expected = f"Neo: {self._neo_line}"
         assert str(sample_quote) == expected
 
     def test_unattributed_line(self):
