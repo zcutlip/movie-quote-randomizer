d49a9a9a04ea7305cdc14745cbce1cb64e1407ec
Zachary Cutlip <uid000@gmail.com>
2025-07-08 22:33:03 -0700

    make validate_quotes_json() return bool

--- mq_randomizer/json_validation.py
+++ mq_randomizer/json_validation.py
@@ -2,15 +2,34 @@ import json
 
 try:
     from jsonschema import validate as jsonschema_validate
+    VALIDATION_ENABLED = True
 except ImportError:
     def jsonschema_validate(*args, **kwargs) -> None:
         pass
+    VALIDATION_ENABLED = False
 
 from . import data
 from .pkg_resources import data_location_as_path
 
 
-def validate_quotes_json(obj: object):
+def validate_quotes_json(obj: object) -> bool:
+    """
+    Validate quote JSON data
+
+    NOTE: If jsonschema is not installed, this function
+          turns into a no-op and has no effect. It returns False
+          in this case
+    Parameters
+    ----------
+    obj : object
+        The quotes object unserialized from JSON
+
+    Returns
+    -------
+    bool
+        Whether validation was enabled and actually occured, or was a no-op
+    """
     schema_src = data_location_as_path(data, data.QUOTES_SCHEMA_JSON)
     schema = json.load(open(schema_src))
-    return jsonschema_validate(obj, schema)
+    jsonschema_validate(obj, schema)
+    return VALIDATION_ENABLED
